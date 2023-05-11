aa3d23262602874524893f1a210a6d820d4d4ef2
osh <osh8242@gmail.com>
2023-05-11 22:40:57 +0900

    feat: 그림판 v1.0 완성

---
+++ WhoWeAre2/src/main/webapp/pages/OSH_canvas.js
@@ -0,0 +1,35 @@
+let cv = document.getElementById("myCanvas");
+let obj = cv.getContext("2d"); // 그림 객체
+let startX, startY; // 시작좌표
+let isDrawing = false; // 그리기 상태
+
+
+cv.addEventListener('mousedown', function(e) {mouseDown(e)}, false);
+cv.addEventListener('mousemove', function(e) {mouseMove(e)}, false);
+cv.addEventListener('mouseup', function(e) {mouseUp(e)}, false);
+
+function draw(curX, curY){
+  obj.beginPath();
+  obj.strokeStyle = document.getElementById("selColor").value;
+  obj.lineWidth = document.getElementById("selLineWidth").value;
+	obj.moveTo(startX, startY);
+	obj.lineTo(curX, curY);
+	obj.stroke();
+}
+
+function mouseDown (e){
+  isDrawing = true;
+  startX = e.offsetX;
+  startY = e.offsetY;
+}
+function mouseMove(e){
+  if(!isDrawing) return;
+  let curX = e.offsetX;
+  let curY = e.offsetY;
+  draw(curX, curY);
+  startX = curX;
+  startY = curY;  
+}
+function mouseUp(){
+  isDrawing = false;
+}
